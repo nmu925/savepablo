8804a32abe59fc8e8b17884ff18dc974f920bfe5
sumkit <skitahar@andrew.cmu.edu>
2016-03-18 22:46:01 -0400

    user model

--- webapp/savepablo/models.py
+++ webapp/savepablo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# User class for built-in authentication module
+from django.contrib.auth.models import User
+
+class MyUser(models.Model):
+  user = models.ForeignKey(MyUser)
+  points = models.DecimalField(max_digits=11,decimal_places=0, default=0, blank=True)
+  def __unicode__(self):
+  	return self.user.username
+  def __str__(self):
+  	return self.__unicode__()
