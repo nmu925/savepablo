3dc0a64c7bf9200e00aa0a9112d0b2e4c58b622c
Nicholas Mu <nicholasmu@Nicholass-MBP-2.wv.cc.cmu.edu>
2016-03-20 17:24:55 -0400

    added friends and items field to user

--- webapp/savepablo/models.py
+++ webapp/savepablo/models.py
@@ -4,9 +4,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MyUser(models.Model):
-  user = models.ForeignKey(MyUser)
+  user = models.ForeignKey(User)
   points = models.DecimalField(max_digits=11,decimal_places=0, default=0, blank=True)
+  friends = models.ManyToManyField("self",symmetrical=True)
+
+  #Represents the quantity of each item/powerup, total of 10 items for now
+  items = models.CommaSeparatedIntegerField(max_length=10)
+
+
   def __unicode__(self):
   	return self.user.username
   def __str__(self):
   	return self.__unicode__()
+
+
+
