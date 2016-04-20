db39a60a0c6c94eda906f1463ab3054f75b44da9
Ubuntu <ubuntu@ip-172-31-14-166.us-west-2.compute.internal>
2016-04-20 18:56:41 +0000

    Ec2 server settings/setup

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.msyql',
+        'NAME': 'savepablo',
+	'USER': 'ROOT',
+	'PASSWORD': 'mujob'
     }
 }
 
